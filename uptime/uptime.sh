4cc2431c24f291c3517bd8354ca74870a7ed8e14
kesavan-m17 <107992893+kesavan-m17@users.noreply.github.com>
2023-04-19 20:44:25 +0530

    Update uptime.sh

--- uptime/uptime.sh
+++ uptime/uptime.sh
@@ -1,34 +1,91 @@
 #!/bin/bash
 
-APP=$1 # <Url Service name>
-URL=$2 #'https://your-service-url.com'
-ACCEPTED_CODE=$3  #200
+set -e
 
-hit(){
-  STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $URL | grep -v %)
+# Export Variables
+CURRENT=$(date +"%Y-%m-%d %H:%M:%S")
+URL='http://localhost'
+APP=my-uptime
+
+ping(){
+    STATUS_CODE=$(curl -sI $URL | grep HTTP | cut -d " " -f 2)
 }
-cal_downtime(){  
-  MPHR=60    # Minutes per hour.
-  DOWNTIME=$(( ($UP - $DOWN) / $MPHR ))
+# Function to calculate time difference
+time_diff(){
+    # Convert the date-time values to Unix timestamps
+    START_TIME=$(date -d "$START" +%s)
+    END_TIME=$(date -d "$END" +%s)
+
+    # Calculate the time difference in seconds
+    time_difference=$((END_TIME - START_TIME))
+
+    hours=$((time_difference / 3600))
+    minutes=$(( (time_difference % 3600) / 60 ))
+    seconds=$((time_difference % 60))
+
+    # Format the DOWNTIME
+    output=""
+    if [ $hours -gt 0 ]; then
+        output+=" $hours-hour"
+    fi
+    if [ $minutes -gt 0 ]; then
+        output+=" $minutes-min"
+    fi
+    if [ $seconds -gt 0 ]; then
+        output+=" $seconds-sec"
+    fi
+
+    # Print the time difference
+    echo "The APP Down Time = $output"
+    DOWNTIME=$output    
 }
 
-hit
-IS_DOWN=$(cat /tmp/is-downtime.txt)
+# Function to handle UP status
+UP(){
+    if [[ $(cat /tmp/STATUS.txt) == "UP" ]]; then
+        echo "UP=true" > /tmp/STATUS.txt
+        END=$(date "+%Y-%m-%d %H:%M:%S")
+        UP_AT=$(date '+%Y-%m-%d-%H:%M:%S')
+        time_diff
+        # notify-send 'APP is UP'
+        echo "$DOWNTIME"
+        uptime-monitoring/google-chat-notify/gchat.sh up $APP $DOWN_AT $UP_AT $DOWNTIME        
+        exit 0            
+    fi
+    if [[ $(cat /tmp/STATUS.txt) == "UP=true" ]]; then
+        echo "Already UP"
+        exit 0
+    fi
+}
 
-if [[ $STATUS_CODE != $ACCEPTED_CODE && $IS_DOWN == " " && "! ${ACCEPTED_CODE[*]} " =~ " ${STATUS_CODE} "]];then    
-  DOWN_AT=$(date '+%Y-%m-%d-%H:%M:%S')
-  DOWN_SEC=$(date +%s)
-  echo "$DOWN_SEC" > /tmp/is-downtime.txt
-  echo "$DOWN_AT" > /tmp/downAt.txt
-  uptime-monitoring/google-chat-notify/gchat.sh down $APP $DOWN_AT  
-fi
-if [[ $STATUS_CODE == $ACCEPTED_CODE && $IS_DOWN != " " && " ${ACCEPTED_CODE[*]} " =~ " ${STATUS_CODE} " ]];then     
-   UP_AT=$(date '+%Y-%m-%d-%H:%M:%S')
-   UP=$(date +%s)
-   DOWN=$(cat /tmp/is-downtime.txt)
-   DOWN_AT=$(cat /tmp/downAt.txt)
-   cal_downtime
-   uptime-monitoring/google-chat-notify/gchat.sh up $APP $DOWN_AT $UP_AT $DOWNTIME-mins
-   echo " " > /tmp/is-downtime.txt
-   exit 0
+# Function to handle DOWN status
+DOWN(){
+    ping    
+    echo "DOWN" > /tmp/STATUS.txt
+    until [[ "$STATUS_CODE" == "200" ]]; do            
+        ping                
+        STATUS=$(cat /tmp/STATUS.txt)
+        if [[ "$STATUS_CODE" == "200" ]]; then
+            echo "UP" > /tmp/STATUS.txt
+            UP
+        fi    
+        case $STATUS in
+            DOWN=true) echo "Already DOWN" > /tmp/null.txt
+            ;;
+            DOWN) 
+            START=$(date "+%Y-%m-%d %H:%M:%S")         
+            DOWN_AT=$(date '+%Y-%m-%d-%H:%M:%S')
+            uptime-monitoring/google-chat-notify/gchat.sh down $APP $DOWN_AT
+            echo "DOWN=true" >> /tmp/STATUS.txt
+            time
+            ;;
+        esac
+        sleep 5
+    done
+}
+ping
+if [[ "$STATUS_CODE" == "200" ]]; then    
+    UP
+else
+    DOWN
 fi
